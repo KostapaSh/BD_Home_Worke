5d9f177dceaa47abc145c530ff63b5dfd6819465
Константин Шульженко <konstantinsulzenko@MacBook-Pro-Konstantin.local>
2022-11-17 19:10:00 +0300

    bd_home_worke_2

---
+++ Script.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS music_genre(
+	id SERIAL RIMARY KEY,
+	title_genre varchar(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS artist(
+	id SERIAL PRIMARY KEY,
+	pseudonym varchar(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS album(
+	id SERIAL PRIMARY KEY,
+	title varchar(60) NOT NULL,
+	year_issue date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genre_artist(
+	id SERIAL PRIMARY KEY,
+	genre_id integer NOT NULL REFERENCES music_genre (id),
+	artist_id integer NOT NULL REFERENCES artist (id)
+);
+
+CREATE TABLE IF NOT EXISTS album_artist(
+    id SERIAL PRIMARY KEY,
+    album_id integer NOT NULL REFERENCES album (id),
+    artist_id integer NOT NULL REFERENCES artist (id)
+);
+
+CREATE TABLE IF NOT EXISTS tracks(
+    id SERIAL PRIMARY KEY,
+    title varchar(60) NOT NULL,
+    duration integer NOT NULL,
+    album_id integer NOT NULL REFERENCES album (id)
+);
+
+CREATE TABLE IF NOT EXISTS collection(
+    id SERIAL PRIMARY KEY,
+    title varchar(60) NOT NULL,
+    year_issue date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tracks_collection(
+    id SERIAL PRIMARY KEY,
+    tracks_id integer NOT NULL REFERENCES tracks (id),
+    collection_id integer NOT NULL REFERENCES collection (id)
+);
